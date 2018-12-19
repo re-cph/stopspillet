2459ff04b4367a8ff420a0b6c3e5bf3f9e2ed2e3
Christian <Christian@Christians-MacBook-Pro-4.local>
2018-12-19 09:54:08 +0100

    Initial HTML copied from ROFUS.nu (Some content of top is changed)

---
+++ js/webtrends/webtrends.load.js
@@ -0,0 +1,18 @@
+// WebTrends SmartSource Data Collector Tag v10.4.23
+// Copyright (c) 2018 Webtrends Inc.  All rights reserved.
+// Tag Builder Version: 4.1.3.8
+// Created: 2018.06.28
+window.webtrendsAsyncInit=function(){
+    var dcs=new Webtrends.dcs().init({
+        dcsid:"dcs222a7vz1xbblhc1uujwjif_9p5w",
+        domain:"statse.webtrendslive.com",
+        timezone:1,
+        fpcdom:".rofus.nu",
+        plugins:{
+        }
+        }).track();
+};
+(function(){
+    var s=document.createElement("script"); s.async=true; s.src="js/webtrends/webtrends.min.js";    
+    var s2=document.getElementsByTagName("script")[0]; s2.parentNode.insertBefore(s,s2);
+}());
