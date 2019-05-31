e636a70afd7bde6eebcc46fbb7c29795fc9de1c8
Mikkel Wied Frederiksen <mikkel@wied.cc>
2019-05-31 11:20:04 +0200

    Remove service worker and public files

--- src/index.js
+++ src/index.js
@@ -4,8 +4,3 @@ import 'semantic-ui-css/semantic.min.css'
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('embedded-react-app'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
