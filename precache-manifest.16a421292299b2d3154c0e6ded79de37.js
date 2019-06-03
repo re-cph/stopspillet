0ab9b061bc00a1899e06d0e3bccc397cff223158
Thomas Vestergaard <vesters@Pamela.local>
2019-06-03 15:58:18 +0200

    Updates

---
+++ precache-manifest.16a421292299b2d3154c0e6ded79de37.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96eae97583ed9fcb7034296695bf0d17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eeca625e337483c5b1c",
+    "url": "/static/js/2.c3b8c76b.chunk.js"
+  },
+  {
+    "revision": "6606ae79e3e1fea268e9",
+    "url": "/static/js/main.d18a01c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
