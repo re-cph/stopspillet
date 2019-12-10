514c26adee832ddcdced0a65afa3e8ca8867f8d7
Christian Mehlbye <m@re-cph.com>
2019-12-10 11:22:46 +0100

    Updates

---
+++ precache-manifest.409ef17585e0e3762d429d72e5f1c1f4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec725c96c9192637dee69540e119ea7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eeca625e337483c5b1c",
+    "url": "/static/js/2.c3b8c76b.chunk.js"
+  },
+  {
+    "revision": "9fb0a9d76630749a43e1",
+    "url": "/static/js/main.12dce53a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.427388b0d78e8cf98e13121bfe62e799.js"
+  "/precache-manifest.409ef17585e0e3762d429d72e5f1c1f4.js"
 );
 
 self.addEventListener('message', (event) => {
