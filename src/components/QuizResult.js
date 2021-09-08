de20dbc88d5bba9d626f50e5e34dfc63575cae74
Mikkel Wied Frederiksen <mikkel@wied.cc>
2021-09-08 16:07:59 +0200

    Updated quiz result with phone link

--- src/components/QuizResult.js
+++ src/components/QuizResult.js
@@ -4,14 +4,25 @@ import {QuizContext} from '../QuizProvider'
 function resultSelector (count) {
   switch (count) {
     case 0:
-      return 'Testen peger på, at du ikke har et problem med spil. Er du nysgerrig på mere viden om ansvarligt spil, er du velkommen til at kontakte StopSpillet.'
+      return (
+        <p>Testen peger på, at du ikke har et problem med spil. Er du nysgerrig på mere viden om ansvarligt spil, er du velkommen til at kontakte StopSpillet.</p>
+      );
     case 1:
-      return 'Testen peger på, at du kan være i risiko for at udvikle et problem med spil. Du bør derfor være opmærksom på, om dine spillevaner ændrer sig. Er du i tvivl og har brug for vejledning, er du velkommen til at kontakte StopSpillet.'
+      return (
+        <p>Testen peger på, at du kan være i risiko for at udvikle et problem med spil. Du bør derfor være opmærksom på, om dine spillevaner ændrer sig. Er du i tvivl og har brug for vejledning, er du velkommen til at kontakte StopSpillet.</p>
+      );
     case 2:
     case 3:
-      return 'Testen peger på, at du kan have et problem med spil. Dine spilvaner kan udvikle sig til en egentlig afhængighed. Derfor opfordres du til at tage kontakt til StopSpillet, hvor vi kan hjælpe dig med afklaring og sammen finde ud af eventuelle videre tiltag.'
+      return (
+        <p>Testen peger på, at du kan have et problem med spil. Dine spilvaner kan udvikle sig til en egentlig afhængighed. Derfor opfordres du til at tage kontakt til StopSpillet, hvor vi kan hjælpe dig med afklaring og sammen finde ud af eventuelle videre tiltag.</p>
+      );
     default:
-      return 'Testen peger på, at du kan have en spilafhængighed. Du opfordres til at tage kontakt til StopSpillet, hvor vi kan hjælpe dig med afklaring og sammen finde ud af eventuelle videre tiltag.'
+      return (
+        <React.Fragment>
+          <p>Testen peger på, at du kan have en spilafhængighed. Du opfordres til at tage kontakt til StopSpillet, hvor vi kan hjælpe dig med afklaring og sammen finde ud af eventuelle videre tiltag.</p>
+          <p><a href="tel:70222825">Kontakt os nu på 70 22 28 25</a></p>
+        </React.Fragment>
+      )
   }
 }
 
@@ -22,9 +33,7 @@ export default function QuizResult () {
         <React.Fragment>
           <section className='quiz-content-header'>
             <h3>Resultat</h3>
-            <p>
-              {resultSelector(context.state.quiz.filter(val => val === true).length)}
-            </p>
+            {resultSelector(context.state.quiz.filter(val => val === true).length)}
           </section>
 
           <section className='quiz-content-footer'>
