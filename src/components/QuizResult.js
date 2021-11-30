728cd863d9b25df39827feaab00bd73ebec842b7
Christian Mehlbye <m@re-cph.com>
2021-11-30 16:32:43 +0100

    Tilføj telefonnummer i alle svar i test

--- src/components/QuizResult.js
+++ src/components/QuizResult.js
@@ -5,21 +5,21 @@ function resultSelector (count) {
   switch (count) {
     case 0:
       return (
-        <p>Testen peger på, at du ikke har et problem med spil. Er du nysgerrig på mere viden om ansvarligt spil, er du velkommen til at kontakte StopSpillet.</p>
+        <p>Testen peger på, at du ikke har et problem med spil. Er du nysgerrig på mere viden om ansvarligt spil, er du velkommen til at tage kontakt til StopSpillet på <a href="tel:70222825">70 22 28 25</a>.</p>
       );
     case 1:
       return (
-        <p>Testen peger på, at du kan være i risiko for at udvikle et problem med spil. Du bør derfor være opmærksom på, om dine spillevaner ændrer sig. Er du i tvivl og har brug for vejledning, er du velkommen til at kontakte StopSpillet.</p>
+        <p>Testen peger på, at du kan være i risiko for at udvikle et problem med spil. Du bør derfor være opmærksom på, om dine spillevaner ændrer sig. Er du i tvivl og har brug for vejledning, er du velkommen til at tage kontakt til StopSpillet på <a href="tel:70222825">70 22 28 25</a>.</p>
       );
     case 2:
     case 3:
       return (
-        <p>Testen peger på, at du kan have et problem med spil. Dine spilvaner kan udvikle sig til en egentlig afhængighed. Derfor opfordres du til at tage kontakt til StopSpillet, hvor vi kan hjælpe dig med afklaring og sammen finde ud af eventuelle videre tiltag.</p>
+        <p>Testen peger på, at du kan have et problem med spil. Dine spilvaner kan udvikle sig til en egentlig afhængighed. Derfor opfordres du til at tage kontakt til StopSpillet på <a href="tel:70222825">70 22 28 25</a>, hvor vi kan hjælpe dig med afklaring og sammen finde ud af eventuelle videre tiltag.</p>
       );
     default:
       return (
         <React.Fragment>
-          <p>Testen peger på, at du kan have en spilafhængighed. Du opfordres til at tage kontakt til StopSpillet, hvor vi kan hjælpe dig med afklaring og sammen finde ud af eventuelle videre tiltag.</p>
+          <p>Testen peger på, at du kan have en spilafhængighed. Du opfordres til at tage kontakt til StopSpillet på 70 22 28 25, hvor vi kan hjælpe dig med afklaring og sammen finde ud af eventuelle videre tiltag.</p>
           <p><a href="tel:70222825">Kontakt os nu på 70 22 28 25</a></p>
         </React.Fragment>
       )
