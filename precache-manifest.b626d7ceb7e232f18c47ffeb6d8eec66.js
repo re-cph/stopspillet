af61d5bf5958e9ce9349adff566776f8fd43396d
vesters <v@re-cph.com>
2022-06-02 22:31:47 +0200

    Updates

--- precache-manifest.b626d7ceb7e232f18c47ffeb6d8eec66.js
+++ precache-manifest.b626d7ceb7e232f18c47ffeb6d8eec66.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c576d0e3dbf0924abbfe88b14d69d10c",
+    "revision": "60a02154c182867ffefa0a7a2e613a8b",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c3b8c76b.chunk.js"
   },
   {
-    "revision": "2f30c4596508a534d230",
-    "url": "/static/js/main.fd501827.chunk.js"
+    "revision": "796cbf386f44da2deefd",
+    "url": "/static/js/main.5b03b95b.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
